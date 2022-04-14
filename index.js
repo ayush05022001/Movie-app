ce8d740913774ba596eade0e96bc6dd0d2c6dbc3
AYUSH SRIVASTAVA <56442013+ayush05022001@users.noreply.github.com>
2022-04-14 22:22:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+import  '/home/ayush/Desktop/my_first_app/src/App.css'
+
+import App from '/home/ayush/Desktop/my_first_app/src/App.js';
+
+
+ReactDOM.render(<App />,document.getElementById('root'));
